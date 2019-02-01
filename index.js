a33ff67cb72f7f174bcb1ea09cd5d0dacb8cfd0d
Øivind Viktor Jenssen <oivind.jenssen@webstep.no>
2019-02-01 11:04:27 +0100

    fixup mockdata

--- index.js
+++ index.js
@@ -4,27 +4,38 @@ const express = require('express');
 const app = express();
 
 app.get('/api/mapinfo', (req, res) => {
-  const positions = [{
-    lat: 63.43331840936477,
-    lng: 10.403631541901916
+  const rooms = [{
+    id: 'sale',
+    detected: 'Beacon 1'
   }, {
-    lat: 63.433311485041855,
-    lng: 10.403254642923883
+    id: 'storage',
+    detected: 'Beacon 1'
   }, {
-    lat: 63.43328724990229,
-    lng: 10.403397371035766
+    id: 'meeting1',
+    detected: 'Beacon 1'
   }, {
-    lat: 63.43334805409731,
-    lng: 10.403675569887668
+    id: 'kitchen',
+    detected: 'Beacon 1'
+  }, {
+    id: 'toilet',
+    detected: 'Beacon 1'
+  }, {
+    id: 'project',
+    detected: 'Beacon 1'
+  }, {
+    id: 'common',
+    detected: 'Beacon 1'
+  }, {
+    id: 'meeting2',
+    detected: 'Beacon 1'
+  }, {
+    id: 'meeting3',
+    detected: 'Beacon 1'
   }];
-  const positionIndex = Math.floor((Math.random() * positions.length) + 1) - 1;
-  const data = [{
-    deviceId: '1',
-    position: positions[positionIndex],
-    alarmTriggered: positionIndex % 2 === 0 ? true : false,
-    timestamp: Date.now(),
-    accuracy: 0
-  }];
+  const randomIndex = Math.floor((Math.random() * rooms.length) + 1) - 1;
+  const data = {
+    activeRoom: rooms[randomIndex]
+  };
   res.send(data);
 });
 
