6eb5c99d5eb92747ceb31275de72f896dae28f34
Øivind Viktor Jenssen <oivind.jenssen@webstep.no>
2019-02-01 13:58:42 +0100

    add websocket plugin

--- index.js
+++ index.js
@@ -2,41 +2,81 @@ const path = require('path')
 const express = require('express');
 
 const app = express();
+const WebSocket = require('ws');
 
-app.get('/api/mapinfo', (req, res) => {
-  const rooms = [{
-    id: 'sale',
-    detected: 'Beacon 1'
-  }, {
-    id: 'storage',
-    detected: 'Beacon 1'
-  }, {
-    id: 'meeting1',
-    detected: 'Beacon 1'
-  }, {
-    id: 'kitchen',
-    detected: 'Beacon 1'
-  }, {
-    id: 'toilet',
-    detected: 'Beacon 1'
-  }, {
-    id: 'project',
-    detected: 'Beacon 1'
-  }, {
-    id: 'common',
-    detected: 'Beacon 1'
-  }, {
-    id: 'meeting2',
-    detected: 'Beacon 1'
-  }, {
-    id: 'meeting3',
-    detected: 'Beacon 1'
-  }];
-  const randomIndex = Math.floor((Math.random() * rooms.length) + 1) - 1;
-  const data = {
-    activeRoom: rooms[randomIndex]
-  };
-  res.send(data);
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+    const rooms = [{
+      id: 'sale',
+      detected: 'Beacon 1'
+    }, {
+      id: 'storage',
+      detected: 'Beacon 1'
+    }, {
+      id: 'meeting1',
+      detected: 'Beacon 1'
+    }, {
+      id: 'kitchen',
+      detected: 'Beacon 1'
+    }, {
+      id: 'toilet',
+      detected: 'Beacon 1'
+    }, {
+      id: 'project',
+      detected: 'Beacon 1'
+    }, {
+      id: 'common',
+      detected: 'Beacon 1'
+    }, {
+      id: 'meeting2',
+      detected: 'Beacon 1'
+    }, {
+      id: 'meeting3',
+      detected: 'Beacon 1'
+    }];
+    const randomIndex = Math.floor((Math.random() * rooms.length) + 1) - 1;
+    const data = {
+      activeRoom: rooms[randomIndex]
+    };
+
+  ws.send([data]);
 });
 
-app.listen(8081, () => console.log('Server listening on port 8081!'));
+// app.get('/api/mapinfo', (req, res) => {
+//   const rooms = [{
+//     id: 'sale',
+//     detected: 'Beacon 1'
+//   }, {
+//     id: 'storage',
+//     detected: 'Beacon 1'
+//   }, {
+//     id: 'meeting1',
+//     detected: 'Beacon 1'
+//   }, {
+//     id: 'kitchen',
+//     detected: 'Beacon 1'
+//   }, {
+//     id: 'toilet',
+//     detected: 'Beacon 1'
+//   }, {
+//     id: 'project',
+//     detected: 'Beacon 1'
+//   }, {
+//     id: 'common',
+//     detected: 'Beacon 1'
+//   }, {
+//     id: 'meeting2',
+//     detected: 'Beacon 1'
+//   }, {
+//     id: 'meeting3',
+//     detected: 'Beacon 1'
+//   }];
+//   const randomIndex = Math.floor((Math.random() * rooms.length) + 1) - 1;
+//   const data = {
+//     activeRoom: rooms[randomIndex]
+//   };
+//   res.send(data);
+// });
+
+// app.listen(8081, () => console.log('Server listening on port 8081!'));
